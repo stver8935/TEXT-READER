8f8e53e7be0a2de1a43083d0b1fa48650d9c9118
LG <gihyeonghan@gmail.com>
2023-04-27 16:05:36 +0900

    initialize

--- app/src/main/java/com/stver8935/text_reader/ui/main/MainActivity.java
+++ app/src/main/java/com/stver8935/text_reader/ui/main/MainActivity.java
@@ -1,8 +1,10 @@
-package com.stver8935.text_reader;
+package com.stver8935.text_reader.ui.main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.stver8935.text_reader.R;
 
 public class MainActivity extends AppCompatActivity {
 
