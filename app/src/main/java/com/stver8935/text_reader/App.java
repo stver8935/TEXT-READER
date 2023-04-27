4b5dbbf2a6cc8c75fb42577e5c21f02c6e2d2534
LG <gihyeonghan@gmail.com>
2023-04-27 16:03:47 +0900

    initialize

---
+++ app/src/main/java/com/stver8935/text_reader/App.java
@@ -0,0 +1,20 @@
+package com.stver8935.text_reader;
+
+import android.app.Application;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        onInit();
+    }
+
+
+    /**
+     * 초기화
+     * */
+    private void onInit(){
+
+    }
+}
